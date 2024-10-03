081d2bf41d450ef1bf1201b1dd6ae1f151fdad63
larih <mewmewdevart@gmail.com>
2024-10-03 12:34:11 -0300

    feat: organiza the mention of Home page

--- steam-clone-frontend/src/App.tsx
+++ steam-clone-frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import Home from '@components/pages/home'
+
 import './index.css'
 
 function App() {
   return (
     <>
-    <h1 className='text-purple-500 font-bold'>oie</h1>
+      <Home/>
     </>
   )
 }
