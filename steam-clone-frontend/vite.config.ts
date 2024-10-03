0647d4a7410e9ffe14b1755c09cb005674cb85a8
larih <mewmewdevart@gmail.com>
2024-10-03 12:30:21 -0300

    feat: settings - add the structure of project

--- steam-clone-frontend/vite.config.ts
+++ steam-clone-frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@contexts': '/src/contexts',
+      '@hooks': '/src/hooks',
+      '@services': '/src/services',
+      '@routers': '/src/routers',
+      '@pages': '/src/pages',
+      '@config': '/src/config.ts',
+      '@utils': '/src/utils',
+      '@wdyr': '/src/wdyr.ts',
+      '@src': '/src',
+    },
+  },
+});
